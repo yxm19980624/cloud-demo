015583e35cbecadc9376a4e4f41a56b48012760a
yxm19980624 <1136656235@qq.com>
2020-08-11 11:19:26 +0800

    gateway网关实现。

---
+++ cloud2020/cloud-gateway8080/src/main/java/com/atguigu/springcloud/GatewayMain8080.java
@@ -0,0 +1,15 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: yxm
+ * @Date: 2020/8/11 9:42
+ */
+@SpringBootApplication
+public class GatewayMain8080 {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain8080.class,args);
+    }
+}
